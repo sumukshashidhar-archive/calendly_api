749cb779b97d90544312502481cc65600195a62e
sumukshashidhar <sumukuuu@gmail.com>
2021-11-18 03:13:42 -0600

    feat: add basic server with morgan logging

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const fs = require('fs');
+var rfs = require('rotating-file-stream') // version 2.x
+
+// defining the Express app
+const app = express();
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// create a rotating write stream
+var accessLogStream = rfs.createStream('access.log', {
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, 'log')
+})
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
+
+app.get('/test', (req, res) => {
+    res.send("UP");
+});
+
+// starting the server
+app.listen(3001, () => {
+    console.log('listening on port 3001');
+});
